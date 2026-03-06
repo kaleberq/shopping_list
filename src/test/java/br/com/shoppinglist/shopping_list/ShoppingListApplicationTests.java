51167c7af3cf08d2be7f4d9c885c20589688ec37
kaleberq <kalebemisael@gmail.com>
2026-03-06 15:28:35 -0300

    Initial Spring Boot project

---
+++ src/test/java/br/com/shoppinglist/shopping_list/ShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.shoppinglist.shopping_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
